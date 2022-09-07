722e743393643e774058ec348d42661a24ac9abe
Troels Lund <trolund@gmail.com>
2022-09-07 10:46:26 +0200

    ex02 cleanup

--- EX02/one.py
+++ EX02/one.py
@@ -4,15 +4,17 @@ from EX02.event import Event
 def log_as_dictionary(log: str):
     dict = {}
 
+    # iterate over all input lines
     for line in log.splitlines():
 
+        # if is an empty line just skip it
         if len(line) == 0:
             continue
 
+        # split the line by ';'
         parts = line.split(";")
 
-        # print(parts)
-
+        # give the parts readable names
         case_id = parts[1]
         task_name = parts[0]
         user_id = parts[2]
@@ -22,7 +24,10 @@ def log_as_dictionary(log: str):
         if dict.get(case_id) is None:
             dict[case_id] = []
 
+        # put data into a event object
         event = Event(case_id=case_id, name=task_name, date=time, user_id=user_id)
+
+        # append that object to resulting dictionary
         dict[case_id].append(event)
 
     return dict
@@ -37,18 +42,20 @@ def dependency_graph(log):
         # iterate event list of case
         for i in range(len(log[case])-1):
 
-            # create dic if not there
+            # create dic if not there and first task with the occurrence 1
             if dep_graph.get(log[case][i].name) is None:
                 dep_graph[log[case][i].name] = {log[case][i+1].name: 1}
 
-            #
+            # now we know the dict is there! :)
             else:
 
-                if log[case][i+1].name not in dep_graph[log[case][i].name]:
-                    inside_dic = dep_graph[log[case][i].name]
-                    inside_dic[log[case][i+1].name] = 1
+                # get nested dict for the task
+                nested_dic = dep_graph[log[case][i].name]
+
+                # if the task name is not in the task's dict then add it with occurrence 1 else add +1 to it.
+                if log[case][i+1].name not in nested_dic:
+                    nested_dic[log[case][i+1].name] = 1
                 else:
-                    small_dic = dep_graph[log[case][i].name]
-                    small_dic[log[case][i+1].name] += 1
+                    nested_dic[log[case][i+1].name] += 1
 
     return dep_graph
